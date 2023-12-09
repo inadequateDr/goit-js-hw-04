1ce836c1141bf2c485f35b1fa89e1a3a033e07ac
inadequateDr <dreamerinadequate@gmail.com>
2023-12-10 01:42:51 +0200

    all
    all

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,19 @@
-function filterArray(numbers, value) {
-  let filteredArray = [];
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      filteredArray.push(numbers[i]);
-    }
-  }
-
-  return filteredArray;
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(profile.getInfo());
+profile.changeUsername('Marco');
+console.log(profile.getInfo());
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
